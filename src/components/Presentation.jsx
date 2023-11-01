f5671b60c96449b4c3d263052cc6e9c847e47939
jic <ji_camargo@yahoo.com>
2023-11-01 00:58:16 -0500

    compress pictures, midifily social media links to open in a different windows, change intro paragraph

--- src/components/Presentation.jsx
+++ src/components/Presentation.jsx
@@ -4,10 +4,10 @@ import '../css/Presentation.css';
 const PersonalPresentation = () => {
   return (
     <section className="Headline">
-      <h1 className="Primarytext big-title">Hey There.<br />I'm Jorge.</h1>
-      <h2 className="Supportingtext">I'm a Software Developer</h2>
+      <h1 className="Primarytext big-title">Hey There !<br />I'm Jorge Camargo</h1>
+      <h2 className="Supportingtext">Code Artisan</h2>
       <p className="Simpletext">
-        I can help you build a product, feature, or website. Look through some of my work and experience! If you like what you see and have a project you need coded, don’t hesitate to contact me.
+        A seasoned Software Developer with a strong track record. I bring over 6 years of experience to the table, specializing in technologies like JavaScript, React, Ruby on Rails, Database design and more. Explore my portfolio and let's chat about how I can bring your project to life!
       </p>
     </section>
   );
--- src/components/SocialIconsBar.jsx
+++ src/components/SocialIconsBar.jsx
@@ -10,11 +10,11 @@ const SocialIconsBar = ({ location }) => {
 
   return (
     <ul className={iconListClassName}>
-      <li><a href="https://github.com/jicamargo"><FontAwesomeIcon icon={faGithub} className={iconClassName} /></a></li>
-      <li><a href="https://linkedin.com/in/jorgecamargog"><FontAwesomeIcon icon={faLinkedin} className={iconClassName} /></a></li>
-      <li><a href="https://wellfound.com/u/jicamargo"><FontAwesomeIcon icon={faAngellist} className={iconClassName} /></a></li>
-      <li><a href="https://twitter.com/ji_camargo"><FontAwesomeIcon icon={faTwitter} className={iconClassName} /></a></li>
-      <li><a href="https://medium.com/@jicbec"><FontAwesomeIcon icon={faMedium} className={iconClassName} /></a></li>
+      <li><a href="https://github.com/jicamargo" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faGithub} className={iconClassName} /></a></li>
+      <li><a href="https://linkedin.com/in/jorgecamargog" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faLinkedin} className={iconClassName} /></a></li>
+      <li><a href="https://wellfound.com/u/jicamargo" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faAngellist} className={iconClassName} /></a></li>
+      <li><a href="https://twitter.com/ji_camargo" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faTwitter} className={iconClassName} /></a></li>
+      <li><a href="https://medium.com/@jicbec" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faMedium} className={iconClassName} /></a></li>
     </ul>
   );
 };
