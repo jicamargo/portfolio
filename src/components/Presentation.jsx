278bc86aec70a69ff4d86a87ce300ac4e2484b63
jic <ji_camargo@yahoo.com>
2024-01-31 17:32:08 -0500

    refactor cards, add icons to mobile menu, refactor proejcts database file

--- src/components/Presentation.jsx
+++ src/components/Presentation.jsx
@@ -4,10 +4,13 @@ import '../css/Presentation.css';
 const PersonalPresentation = () => {
   return (
     <section className="Headline">
-      <h1 className="Primarytext big-title">Hey There !<br />I'm Jorge Camargo</h1>
-      <h2 className="Supportingtext">Code Artisan</h2>
+      <h3 className='greeting'>
+        <div className="big-title2">Hey There!</div>
+        <div className="big-title">I'm Jorge Camargo</div>
+      </h3>
+      <h2 className="Supportingtext">Web Developer</h2>
       <p className="Simpletext">
-        A seasoned Software Developer with a strong track record. I bring over 6 years of experience to the table, specializing in technologies like JavaScript, React, Ruby on Rails, Database design and more. Explore my portfolio and let's chat about how I can bring your project to life!
+        A seasoned Software Developer with a strong track record. 6+ years of experience, specializing in technologies like Ruby on Rails, JavaScript, React,  Database design and more. Explore my portfolio and let's chat about how I can bring your project to life!
       </p>
     </section>
   );
